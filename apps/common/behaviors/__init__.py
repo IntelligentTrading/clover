64270797aed1e86ea3144c0a9fe76f418fe40674
Tom Counsell <other@tomcounsell.com>
2018-12-05 11:50:34 +0100

    common app

---
+++ apps/common/behaviors/__init__.py
@@ -0,0 +1,16 @@
+from .annotatable import Annotatable
+from .authorable import Authorable
+from .expirable import Expirable
+from .locatable import Locatable
+from .timestampable import Timestampable
+from .uploadable import Uploadable
+
+
+__all__ = [
+    'Annotatable',
+    'Authorable',
+    'Expirable',
+    'Locatable',
+    'Timestampable',
+    'Uploadable',
+]
