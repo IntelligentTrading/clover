c76ab51ee8656cc5044fe438792240d319ec4975
Tom Counsell <other@tomcounsell.com>
2018-12-08 13:20:45 +0100

    placeholder for doge triggers

---
+++ apps/doge/doge_subscribers.py
@@ -0,0 +1,9 @@
+from apps.TA.resources.abstract_subscriber import AbstractSubscriber, SubscriberException
+
+
+class DogeSubscriberException(SubscriberException):
+    pass
+
+
+class DogeSubscriber(AbstractSubscriber):
+    pass
