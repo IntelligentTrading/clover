4c0bee95a43dfa303e19dc94513042da1490cec1
sodogemuchwow <doge1337wow@gmail.com>
2019-01-03 15:34:00 +0100

    Ticker filtering, committee expires.

--- apps/doge/doge_TA_actors.py
+++ apps/doge/doge_TA_actors.py
@@ -3,8 +3,8 @@ from apps.TA.storages.abstract.indicator import IndicatorStorage, BULLISH, BEARI
 from apps.TA.storages.abstract.indicator_subscriber import IndicatorSubscriber
 from apps.TA.indicators.momentum import willr
 from apps.doge.models.doge_train_test import DogeCommittee
-from settings import logger
-
+from settings import logger, SUPPORTED_DOGE_TICKERS
+import time
 
 class SignalSubscriberException(SubscriberException):
     pass
@@ -49,13 +49,28 @@ class DogeSubscriber(SignalSubscriber):
     storage_class = DogeStorage  # override with applicable storage class
 
     def __init__(self, *args, **kwargs):
-        self.committee = DogeCommittee()
-        # todo: set an expiry for the committee on the same schedule as training
-        # todo: ideally, a new training would expire all previous committees, see common.behaviours.expirable
+        self._reload_committee()
         super().__init__(*args, **kwargs)
         logger.info("                                                      (😎 IT IS THE LAST ONE 😎)")
+        logger.info(f'Initialized DogeSubscriber at {time.time()}')
+
+    def _reload_committee(self):
+        self.committee = DogeCommittee()
 
     def handle(self, channel, data, *args, **kwargs):
+        # check if we received data for a ticker we support
+        if self.ticker not in SUPPORTED_DOGE_TICKERS:  # @tomcounsell please check if this is OK or I should register
+                                                       # for tickers of interest in some other way
+            logger.debug(f'Ticker {self.ticker} not in {SUPPORTED_DOGE_TICKERS}, skipping...')
+            return
+
+        # check if the committee has expired
+        if self.committee.expired:
+            logger.info('Doge committee expired, reloading...')
+            self._reload_committee()
+
+        logger.info(f'Doge subscriber invoked at {self.timestamp}, channel={str(channel)}, data={str(data)} '
+                    f'(it is now {time.time()})')
         transaction_currency, counter_currency = self.ticker.split('_')
 
         new_doge_storage = DogeStorage(ticker=self.ticker,
@@ -68,6 +83,7 @@ class DogeSubscriber(SignalSubscriber):
 
         new_doge_storage.value = (sum(ticker_votes) * 100 / len(ticker_votes))  # normalize to +-100 scale
         new_doge_storage.save(publish=True)
+        logger.info('Doge vote saved')
 
 
     def pre_handle(self, channel, data, *args, **kwargs):
