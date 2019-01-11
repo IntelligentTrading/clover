0e66f5c8954ad3388a83ed5f6b207cca2b4369a2
sodogemuchwow <doge1337wow@gmail.com>
2019-01-11 10:00:25 +0100

    Added debug logging, cleanup.

--- apps/doge/doge_train_test.py
+++ apps/doge/doge_train_test.py
@@ -11,9 +11,11 @@ from apps.genetic_algorithms.genetic_program import GeneticTickerStrategy
 from apps.genetic_algorithms.gp_artemis import ExperimentManager
 from apps.genetic_algorithms.leaf_functions import RedisTAProvider
 from apps.TA import PERIODS_1HR
-from settings import DOGE_RETRAINING_PERIOD_SECONDS, logger, SUPPORTED_DOGE_TICKERS, DOGE_LOAD_ROCKSTARS
+from settings import DOGE_RETRAINING_PERIOD_SECONDS, DOGE_TRAINING_PERIOD_DURATION_SECONDS, \
+    logger, SUPPORTED_DOGE_TICKERS, DOGE_LOAD_ROCKSTARS
 import time
 from apps.genetic_algorithms.chart_plotter import save_dot_graph
+from apps.backtesting.utils import datetime_from_timestamp
 
 METRIC_IDS = {
     'mean_profit': 0,
@@ -352,6 +354,9 @@ class DogeSubscriber(SignalSubscriber):
     def _reload_committee(self):
         self.committee = DogeCommittee()
 
+    def _check_committee_expired(self):
+        return self.committee.expired(at_timestamp=self.timestamp)
+
     def handle(self, channel, data, *args, **kwargs):
         # check if we received data for a ticker we support
         if self.ticker not in SUPPORTED_DOGE_TICKERS:  # @tomcounsell please check if this is OK or I should register
@@ -360,7 +365,7 @@ class DogeSubscriber(SignalSubscriber):
             return
 
         # check if the committee has expired
-        if self.committee.expired(at_timestamp=self.timestamp):
+        if self._check_committee_expired():
             logger.info('Doge committee expired, reloading...')
             self._reload_committee()
 
@@ -391,13 +396,16 @@ class DummyDogeSubscriber(DogeSubscriber):
         self.committee = committee
         # no super constructor calls, bypass the Redis and pubsub stuff
 
-    def expired(self, at_timestamp):
+    def _check_committee_expired(self):
         return False
+
 
 
 class DogeHistorySimulator:
 
-    def __init__(self, start_time, end_time, training_period_length, time_to_retrain_seconds, ticker, exchange, horizon):
+    def __init__(self, start_time, end_time, ticker, exchange, horizon,
+                 training_period_length=DOGE_TRAINING_PERIOD_DURATION_SECONDS,
+                 time_to_retrain_seconds=DOGE_RETRAINING_PERIOD_SECONDS):
         self._start_time = db_interface.get_nearest_db_timestamp(start_time, ticker, exchange)
         self._end_time = db_interface.get_nearest_db_timestamp(end_time, ticker, exchange)
         self._training_period_length = training_period_length
@@ -414,8 +422,12 @@ class DogeHistorySimulator:
         for training_end_time in range(self._end_time,
                                        self._start_time + self._training_period_length,
                                        -self._time_to_retrain_seconds):
-            training_end_time = 1547132400  # debug stuff
+            # training_end_time = 1547132400  # debug stuff
             training_start_time = training_end_time - self._training_period_length
+
+            logging.info(f'Processing data for committee trained on data '
+                         f'from {datetime_from_timestamp(training_start_time)} '
+                         f'to {datetime_from_timestamp(training_end_time)}')
 
             # check if a committee record already exists
             try:
@@ -434,8 +446,6 @@ class DogeHistorySimulator:
                                     committee_valid_from=training_end_time,
                                     committee_valid_to=training_end_time + self._time_to_retrain_seconds)
 
-
-
     def feed_price_to_doge(self, committee, committee_valid_from, committee_valid_to):
         prices_df = db_interface.get_resampled_prices_in_range(
             start_time=committee_valid_from,
@@ -449,7 +459,9 @@ class DogeHistorySimulator:
         subscriber = DummyDogeSubscriber(committee)
 
         for row in prices_df.itertuples():
-            logging.info(f'Feeding {str(row)} to doge...')
+            logging.info(f'Feeding {str(row)} to doge (using committee '
+                         f'valid from {datetime_from_timestamp(committee_valid_from)} '
+                         f'to {datetime_from_timestamp(committee_valid_to)}...')
             data_event = {
                 'type': 'message',
                 'pattern': None,
