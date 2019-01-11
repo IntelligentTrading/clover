83f238ec691b1b2980f36c226cdc082a3660bb4b
sodogemuchwow <doge1337wow@gmail.com>
2019-01-11 14:53:20 +0100

    Moved history related stuff, added utils to test performance.

--- apps/doge/doge_train_test.py
+++ apps/doge/doge_train_test.py
@@ -4,15 +4,13 @@ from apps.backtesting.data_sources import db_interface
 from apps.backtesting.tick_listener import TickListener
 from apps.backtesting.tick_provider_heartbeat import TickProviderHeartbeat
 from apps.backtesting.tick_provider import TickerData
-from apps.backtesting.utils import datetime_from_timestamp
 from apps.doge.doge_TA_actors import DogeStorage, DogePerformance, CommitteeStorage, SignalSubscriber, \
     CommitteeVoteStorage
 from apps.genetic_algorithms.genetic_program import GeneticTickerStrategy
 from apps.genetic_algorithms.gp_artemis import ExperimentManager
 from apps.genetic_algorithms.leaf_functions import RedisTAProvider
 from apps.TA import PERIODS_1HR
-from settings import DOGE_RETRAINING_PERIOD_SECONDS, DOGE_TRAINING_PERIOD_DURATION_SECONDS, \
-    logger, SUPPORTED_DOGE_TICKERS, DOGE_LOAD_ROCKSTARS
+from settings import DOGE_RETRAINING_PERIOD_SECONDS, logger, SUPPORTED_DOGE_TICKERS, DOGE_LOAD_ROCKSTARS
 import time
 from apps.genetic_algorithms.chart_plotter import save_dot_graph
 from apps.backtesting.utils import datetime_from_timestamp
@@ -84,7 +82,6 @@ class DogeTrainer:
         # DEBUG: loading rockstars
         rockstars = []
         if DOGE_LOAD_ROCKSTARS:
-            from settings import DOGE_MAX_ROCKSTARS
             rockstars = CommitteeStorage.load_rockstars(num_previous_committees_to_search=2, max_num_rockstars=5,
                                                         ticker='BTC_USDT', exchange='binance', timestamp=end_timestamp)
             logging.info(f'Loaded {len(rockstars)} rockstars.')
@@ -388,91 +385,5 @@ class DogeSubscriber(SignalSubscriber):
 
     def pre_handle(self, channel, data, *args, **kwargs):
         super().pre_handle(channel, data, *args, **kwargs)
-
-
-class DummyDogeSubscriber(DogeSubscriber):
-
-    def __init__(self, committee):
-        self.committee = committee
-        # no super constructor calls, bypass the Redis and pubsub stuff
-
-    def _check_committee_expired(self):
-        return False
-
-
-
-class DogeHistorySimulator:
-
-    def __init__(self, start_time, end_time, ticker, exchange, horizon,
-                 training_period_length=DOGE_TRAINING_PERIOD_DURATION_SECONDS,
-                 time_to_retrain_seconds=DOGE_RETRAINING_PERIOD_SECONDS):
-        self._start_time = db_interface.get_nearest_db_timestamp(start_time, ticker, exchange)
-        self._end_time = db_interface.get_nearest_db_timestamp(end_time, ticker, exchange)
-        self._training_period_length = training_period_length
-        self._time_to_retrain_seconds = time_to_retrain_seconds
-        self._ticker = ticker
-        self._exchange = exchange
-        self._transaction_currency, self._counter_currency = ticker.split('_')
-        self._horizon = horizon
-
-
-    def fill_history(self):
-        karen = DogeTrainer(database=db_interface)  # see Karen Pryor
-
-        for training_end_time in range(self._end_time,
-                                       self._start_time + self._training_period_length,
-                                       -self._time_to_retrain_seconds):
-            # training_end_time = 1547132400  # debug stuff
-            training_start_time = training_end_time - self._training_period_length
-
-            logging.info(f'Processing data for committee trained on data '
-                         f'from {datetime_from_timestamp(training_start_time)} '
-                         f'to {datetime_from_timestamp(training_end_time)}')
-
-            # check if a committee record already exists
-            try:
-                committee = DogeCommittee(committee_timestamp=training_end_time)
-                logging.info(f'Committee successfully loaded at {training_end_time}')
-            except:
-                # no committee, we need to rerun training
-                logging.info(f'No committee found, running training for timestamp {training_end_time}...')
-                karen.retrain_doges(start_timestamp=training_start_time, end_timestamp=training_end_time)
-
-                # now that Karen did her job we should totally have a working committee
-                committee = DogeCommittee(committee_timestamp=training_end_time)
-
-            # we need to simulate incoming price data
-            self.feed_price_to_doge(committee=committee,
-                                    committee_valid_from=training_end_time,
-                                    committee_valid_to=training_end_time + self._time_to_retrain_seconds)
-
-    def feed_price_to_doge(self, committee, committee_valid_from, committee_valid_to):
-        prices_df = db_interface.get_resampled_prices_in_range(
-            start_time=committee_valid_from,
-            end_time=committee_valid_to,
-            transaction_currency=self._transaction_currency,
-            counter_currency=self._counter_currency,
-            horizon=self._horizon
-        )
-
-        # rudely hijack the DogeSubscriber class
-        subscriber = DummyDogeSubscriber(committee)
-
-        for row in prices_df.itertuples():
-            logging.info(f'Feeding {str(row)} to doge (using committee '
-                         f'valid from {datetime_from_timestamp(committee_valid_from)} '
-                         f'to {datetime_from_timestamp(committee_valid_to)}...')
-            data_event = {
-                'type': 'message',
-                'pattern': None,
-                'channel': b'PriceStorage',
-                'data': f'''{{
-                                "key": "{self._ticker}:{self._exchange}:PriceStorage:1",
-                                "name": "{row.close_price}:{row.score}",
-                                "score": "{row.score}"
-                            }}'''.encode(encoding='UTF8')
-            }
-            # call the doge with this event
-            subscriber(data_event=data_event)
 
 
