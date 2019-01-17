62ba6e9a46d848c9f58c070c252eea3008a7c367
sodogemuchwow <doge1337wow@gmail.com>
2019-01-17 17:10:11 +0100

    Run training for all supported tickers.

--- apps/doge/management/commands/train_doge.py
+++ apps/doge/management/commands/train_doge.py
@@ -1,15 +1,17 @@
 from django.core.management.base import BaseCommand
 from apps.doge.doge_train_test import DogeTrainer
+from settings import SUPPORTED_DOGE_TICKERS
 import time
 
 ONE_WEEK = 60*60*24*7
 ONE_DAY = 60*60*24
 ONE_HOUR = 60*60
 
+
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
         end_timestamp = int(time.time())  # UTC timestamp
         start_timestamp = end_timestamp - ONE_HOUR
-        ticker = 'ETH_USDT'
-        DogeTrainer.run_training(start_timestamp, end_timestamp, ticker)
+        for ticker in SUPPORTED_DOGE_TICKERS:
+            DogeTrainer.run_training(start_timestamp, end_timestamp, ticker)
