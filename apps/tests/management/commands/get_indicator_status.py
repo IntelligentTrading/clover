5d6f9a741037e0a85fa2d1102b8506c8b1fac0fa
sodogemuchwow <doge1337wow@gmail.com>
2019-01-21 12:27:35 +0100

    Added a management command to show indicator status, cleanup.

--- apps/tests/management/commands/get_indicator_status.py
+++ apps/tests/management/commands/get_indicator_status.py
@@ -1,12 +1,14 @@
 from django.core.management.base import BaseCommand
-from apps.tests.manual_scripts import RedisTests
-
+from apps.tests.manual_scripts import show_indicator_status
 
 class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument('--ticker', type=str)
+        parser.add_argument('--indicator_key', type=str)
 
     def handle(self, *args, **options):
         ticker = options['ticker'] or 'BTC_USDT'
-        RedisTests.test_ticker_storages(ticker)
+        storage = options['indicator_key'] or 'PriceStorage'
+
+        show_indicator_status(storage, ticker)
