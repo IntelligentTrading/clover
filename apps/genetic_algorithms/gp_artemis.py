b97dfc6909b334af5143e28b803cbf3a025093dd
sodogemuchwow <doge1337wow@gmail.com>
2019-01-22 18:00:19 +0100

    Enabled running experiment variants in parallel.

--- apps/genetic_algorithms/gp_artemis.py
+++ apps/genetic_algorithms/gp_artemis.py
@@ -52,10 +52,11 @@ class ExperimentManager:
 
 
     def __init__(self, experiment_container, read_from_file=True, database=db_interface, hof_size=10,
-                 function_provider=None, rockstars=[]):
+                 function_provider=None, rockstars=[], parallel_run=False):
         self.database = database
         self.hof_size = hof_size
         self.rockstars = rockstars
+        self._parallel_run = parallel_run
 
         if read_from_file:
             with open(experiment_container) as f:
@@ -142,16 +143,23 @@ class ExperimentManager:
 
         return variant.run(keep_record=keep_record, display_results=display_results, saved_figure_ext=saved_figure_ext)
 
+    def run_experiments(self, rerun_existing=False, display_results=True, keep_record=True):
+        if self._parallel_run:
+            self.run_parallel_experiments(rerun_existing, display_results, keep_record)
+        else:
+            self.run_sequential_experiments(rerun_existing, display_results, keep_record)
+
     @time_performance
-    def run_parallel_experiments(self, num_processes=8, rerun_existing=False, display_results=True):
-        partial_run_func = partial(ExperimentManager.run_variant, keep_record=True, display_results=display_results,
+    def run_parallel_experiments(self, rerun_existing=False, display_results=True, keep_record=True):
+        partial_run_func = partial(ExperimentManager.run_variant, keep_record=keep_record, display_results=display_results,
                            rerun_existing=rerun_existing, saved_figure_ext='.fig.png')
 
         records = parallel_run(partial_run_func, self.variants)
         return records
 
     @time_performance
-    def run_experiments(self, rerun_existing=False, display_results=True, keep_record=True):
+    def run_sequential_experiments(self, rerun_existing=False, display_results=True, keep_record=True):
+        records = []
         for i, variant in enumerate(self.variants):
             if len(variant.get_records(only_completed=True)) > 0 and not rerun_existing:
                 logging.info(f">>> Variant {variant.name} already has completed records, skipping...")
@@ -159,6 +167,8 @@ class ExperimentManager:
 
             logging.info(f"Running variant {i}")
             record = variant.run(keep_record=keep_record, display_results=display_results, saved_figure_ext='.fig.png')
+            records.append(record)
+        return records
 
 
     def explore_records(self, use_validation_data=True):
