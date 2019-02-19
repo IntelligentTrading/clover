7ee745365686ef9670bada85cf54895ef989a859
sodogemuchwow <doge1337wow@gmail.com>
2019-02-19 14:16:58 +0100

    Add connections to Postgres to get price data.

--- apps/backtesting/backtester_portfolio.py
+++ apps/backtesting/backtester_portfolio.py
@@ -1,16 +1,18 @@
 import logging
 from apps.backtesting.data_sources import DB_INTERFACE
+from apps.backtesting.legacy_postgres import PostgresDatabaseConnection
 from collections import namedtuple, OrderedDict
 
 Allocation = namedtuple('Allocation', 'amount coin portion unit_price value')
 
+POSTGRES = PostgresDatabaseConnection()
 
 class PortfolioSnapshot:
 
-    def __init__(self, timestamp, allocations_dict, db_interface=DB_INTERFACE):
+    def __init__(self, timestamp, allocations_dict, db_interface=POSTGRES):
         self._timestamp = timestamp
         self._allocations = {}
-        self.db_interface = DB_INTERFACE
+        self.db_interface = db_interface
         self._fill_internals(allocations_dict)
 
     def _fill_internals(self, allocations_dict):
@@ -30,13 +32,24 @@ class PortfolioSnapshot:
     def _get_price(self, coin):
         if coin == 'BTC':
             return 1
-        return self.db_interface.get_price(transaction_currency=coin, timestamp=self._timestamp, timestamp_tolerance=1000)
+        elif coin == 'USDT':
+            # get BTC_USDT price and invert
+            btc_usdt_price = self.db_interface.get_price_nearest_to_timestamp(currency='BTC',
+                                                                    timestamp=self._timestamp,
+                                                                    source=2,  # Binance
+                                                                    counter_currency='USDT')
+            return 1.0 / btc_usdt_price
+
+        return self.db_interface.get_price_nearest_to_timestamp(currency=coin,
+                                                                timestamp=self._timestamp,
+                                                                source=2,               # Binance
+                                                                counter_currency='BTC')
 
     def get_allocation(self, coin):
         return self._allocations.get(coin, None)
 
     def report(self):
-        logging.info(f'At timestamp {timestamp}, portfolio stats are:')
+        logging.info(f'At timestamp {self._timestamp}, portfolio stats are:')
         logging.info(f'    -> total value: {self._total_value}')
         for coin, allocation in self._allocations.items():
             logging.info(f'       {allocation.amount} {allocation.coin} worth {allocation.value} BTC, {allocation.portion*100:2}% total')
@@ -73,7 +86,7 @@ class DummyDataProvider:
         "portion": 0.0034
     },{
         "amount": 0.005,
-        "coin": "BCC",
+        "coin": "OMG",
         "portion": 0.0246
     },{
         "amount": 17.19363945,
@@ -89,10 +102,11 @@ class DummyDataProvider:
 
     def run(self):
         import json, time
+        from apps.backtesting.utils import datetime_to_timestamp
         backtester = PortfolioBacktester()
-        timestamp = int(time.time())
+        timestamp = datetime_to_timestamp('2018/06/01 00:00:00 UTC')
         for i in range(10):
-            backtester.process_allocations(timestamp+i, json.loads(self.sample_allocations))
+            backtester.process_allocations(timestamp+i*60*60*24, json.loads(self.sample_allocations))
         backtester.value_report()
 
 
