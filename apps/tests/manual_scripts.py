e1ca9acc31eee4a20e3fc4c2d7e5a977732c628e
Tom Counsell <other@tomcounsell.com>
2019-04-09 13:52:08 +0200

    progress on fill gaps

--- apps/tests/manual_scripts.py
+++ apps/tests/manual_scripts.py
@@ -66,33 +66,3 @@ class RedisTests:
                 except Exception as e:
                     logging.info(f'Error decoding input: {str(e)}')
         return
-
-
-    @staticmethod
-    def find_gaps(key_pattern, start_timestamp, end_timestamp):
-
-        start_score = PriceStorage.score_from_timestamp(start_timestamp)
-        end_score = PriceStorage.score_from_timestamp(end_timestamp)
-
-        keys = database.keys(key_pattern)
-        for key in keys:
-            logging.info(f'Processing data for {key}...')
-            values = database.zrangebyscore(key, min=start_score, max=end_score)
-            gaps = []
-            for i, item in enumerate(values):
-                if i == len(values) - 1:  # the last element
-                    break
-                current_score = int(item.decode('UTF8').split(':')[-1])
-                next_score = int(values[i + 1].decode('UTF8').split(':')[-1])
-                if next_score == current_score:
-                    logging.warning(f'     Encountered duplicate scores: {item} and {values[i + 1]}')
-                    continue
-                if next_score != current_score + 1:
-                    gaps.append((current_score, next_score))
-
-            logging.info('Found gaps: ')
-
-            for gap in gaps:
-                start = datetime_from_timestamp(PriceStorage.timestamp_from_score(gap[0]))
-                end = datetime_from_timestamp(PriceStorage.timestamp_from_score(gap[1]))
-                logging.info(f'    start: {start}, end: {end}  (scores {gap[0]}-{gap[1]})')
